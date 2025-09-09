5346a7930a1187da35f5f3b86af5a5bdfd7d6ee3
Jeferson Alves <jefersonalvessantos62@gmail.com>
2025-09-09 10:22:28 -0300

    feat: organização das pastas.

---
+++ src/asset/js/api.js
@@ -0,0 +1,18 @@
+export async function api(caminho) {
+  try {
+    const response = await fetch(
+      `https://economia.awesomeapi.com.br/json/last/${caminho}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Erro ${response.status}: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    alert("Erro ao buscar as fantasias:", error.message);
+    return null;
+  }
+}
+
